ebb7904aa8c6e50a0a6d050aa676271d7209044c
chad hinds <chadrickj8@gmail.com>
2023-01-18 14:10:32 -0500

    Add .env to gitignore file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createClient } from '@supabase/supabase-js';
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import reportWebVitals from './reportWebVitals';
+
+const supabase = createClient('https://ahefnrxrupduqwrwinft.supabase.co', process.env.REACT_APP_API_KEY)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
