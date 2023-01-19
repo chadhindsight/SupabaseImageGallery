f438ae60d91388efc9f4cbeee9fe70be0f00932e
chad hinds <chadrickj8@gmail.com>
2023-01-19 14:55:48 -0500

    Add some error handling

--- src/App.js
+++ src/App.js
@@ -10,6 +10,19 @@ function App() {
   const user = useUser();
   const supabase = useSupabaseClient();
 
+  async function magicLinkLogin() {
+    const { data, error } = await supabase.auth.signInWithOtp({
+      email
+    })
+
+    if (error) {
+      alert("There was an error communicating with Supabase, double check email")
+      console.log(error);
+    } else {
+      alert("Check your email for a Supabase Magic Link to log in")
+    }
+  }
+
   return (
     <Container align="center" className="container-sm mt-4">
       {
