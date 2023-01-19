8a569e1db90af0a737ee041a5fcf0f678aa5cac0
chad hinds <chadrickj8@gmail.com>
2023-01-19 14:48:17 -0500

    Add required imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import './App.css';
+import { Container, Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
 
 function App() {
-  return (
-    <div className="App">
+  const [email, setEmail] = useState("");
+  const supabase = useSupabaseClient();
 
-    </div>
+  return (
+    <Container align="center" className="container-sm mt-4">
+      {
+
+      }
+    </Container>
   );
 }
 
